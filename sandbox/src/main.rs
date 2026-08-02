d5142960e12484095547c6471c80a1a4d7f19e40
amrxtgh <ghimireamrit619@gmail.com>
2026-08-03 00:37:23 +0545

    test

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,8 @@
+use forge::log::Logger;
 fn main() {
+    Logger::init();
+    Logger::info("Engine started");
+    Logger::warn("Texture missing");
+    Logger::error("Renderer crashed");
     forge::print();
 }
