8b21c9103e35189f183aafe8f4e160d636219819
amrxtgh <ghimireamrit619@gmail.com>
2026-08-04 17:38:19 +0545

    small change here too

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,9 +1,9 @@
 use forge::log::Logger;
 fn main() {
+    forge::print();
     Logger::init();
     Logger::core_info("Engine started");
     Logger::core_warn("Texture missing");
     Logger::client_error("Renderer crashed");
     Logger::client_debug("Renderer crashed");
-    forge::print();
 }
