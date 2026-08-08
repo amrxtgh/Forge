228cfd3578599613b6dc9d1b05c52fcd06bf06d1
amrxtgh <ghimireamrit619@gmail.com>
2026-08-09 01:17:16 +0545

    change lib.rs

--- forge/src/lib.rs
+++ forge/src/lib.rs
@@ -1,4 +1,27 @@
+pub mod event;
+use crate::event::Event;
 pub mod log;
-pub fn print() {
-    println!("Hello from Forge Engine");
+
+pub struct Window {
+    pub title: String,
+}
+impl Window {
+    pub fn new(title: &str) -> Self {
+        Self {
+            title: title.to_string(),
+        }
+    }
+
+    pub fn process_events<F>(&mut self, mut event_callback: F)
+    where
+        F: FnMut(Event),
+    {
+        let mock_os_close = Event::WindowClose;
+        let mock_os_input = Event::KeyPressed {
+            key: event::KeyCode::Escape,
+            repeat_count: 0,
+        };
+        event_callback(mock_os_close);
+        event_callback(mock_os_input);
+    }
 }
