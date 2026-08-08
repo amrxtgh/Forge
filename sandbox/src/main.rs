5fde4828be056fc3390a336e6bbbb7336321427d
amrxtgh <ghimireamrit619@gmail.com>
2026-08-09 04:13:21 +0545

    todo

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,63 +1,36 @@
 use forge::{
-    Window,
-    event::{self, Event, EventDispatcher},
+    event::{Event, EventDispatcher, KeyCode, KeyPressedEvent, WindowResizeEvent},
 };
+
 struct Application {
-    window: Window,
     running: bool,
 }
 impl Application {
-    pub fn new() -> Self {
-        Self {
-            window: Window::new("Forge Engine v0.1"),
-            running: true,
-        }
-    }
-
-    // create a EventDispatcher around the event and called dispatch with closure
     pub fn on_event(&mut self, mut event: Event) {
         println!("Engine Received Event: {:?}", event);
 
         let mut dispatcher = EventDispatcher::new(&mut event);
 
-        dispatcher.dispatch(|e| match e {
-            Event::WindowClose => {
-                self.on_window_close();
-                true
-            }
-            _ => false,
-        });
-        dispatcher.dispatch(|e| match e {
-            Event::KeyPressed {
-                key: event::KeyCode::Escape,
-                ..
-            } => {
+        dispatcher.dispatch::<KeyPressedEvent, _>(|e| {
+            println!("Key caught by dispatcher: {:?}", e.key);
+            if e.key == KeyCode::Escape {
                 println!("Escape key hit! Exiting application loop");
                 self.running = false;
-                true
+                return true; // Event is marked handled and consumed
             }
-            _ => false,
+            false
         });
-    }
-    fn on_window_close(&mut self) -> bool {
-        println!("Closing main window frame");
-        self.running = false;
-        true
-    }
 
-    // Opens the window and loops while running
-    fn run(&mut self) {
-        println!("Forge Sandbox application running");
-        // create events and then event goes to Application::on_events(e)
-        while self.running {
-            for event in self.window.process_events() {
-                self.on_event(event);
-            }
-        }
+        dispatcher.dispatch::<WindowResizeEvent, _>(|e| {
+            println!("Viewport sizing to: {}x{}", e.width, e.height);
+            true
+        });
     }
 }
 
 fn main() {
-    let mut app = Application::new();
-    app.run();
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Forge Engine", 1280, 720).position
 }
