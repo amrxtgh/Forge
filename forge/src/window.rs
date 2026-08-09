7aca396dd45163baf1ef583d1afb1bbbccfc74b1
amrxtgh <ghimireamrit619@gmail.com>
2026-08-09 15:25:22 +0545

    update: window.rs

--- forge/src/window.rs
+++ forge/src/window.rs
@@ -11,7 +11,7 @@ pub struct WindowProps {
 impl Default for WindowProps {
     fn default() -> Self {
         Self {
-            title: String::from("Forge Engine"),
+            title: String::from("FORGE ENGING"),
             width: 1280,
             height: 720,
         }
@@ -19,15 +19,17 @@ impl Default for WindowProps {
 }
 pub struct Window {
     _sdl_context: Sdl,
-    sdl_window: Sdl3Window,
     pub canvas: Canvas<Sdl3Window>,
     pub event_pump: sdl3::EventPump,
 }
 
 impl Window {
     pub fn new(props: WindowProps) -> Self {
+        // initialize platform layers
         let sdl_context = sdl3::init().expect("Failed to initialize SDL3");
         let video_subsystem = sdl_context.video().expect("Failed to initialize video");
+
+        //building the physical os window
         let sdl_window = video_subsystem
             .window(&props.title, props.width, props.height)
             .position_centered()
@@ -35,18 +37,19 @@ impl Window {
             .build()
             .expect("Failed to create SDL3 Window");
 
+        // create the rendering canvas
         let canvas = sdl_window.clone().into_canvas();
         let event_pump = sdl_context.event_pump().expect("Failed to get event pump");
+
         Self {
             _sdl_context: sdl_context,
-            sdl_window,
             canvas,
             event_pump,
         }
     }
     pub fn on_update(&mut self) {
         self.canvas
-            .set_draw_color(sdl3::pixels::Color::RGB(24, 28, 36));
+            .set_draw_color(sdl3::pixels::Color::RGB(255, 105, 180));
         self.canvas.clear();
         self.canvas.present();
     }
