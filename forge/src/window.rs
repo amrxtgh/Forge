580c0c621c7bb09c1b29fa460fafd90fbb3ee7f2
amrxtgh <ghimireamrit619@gmail.com>
2026-08-09 14:09:35 +0545

    update: window.rs

--- forge/src/window.rs
+++ forge/src/window.rs
@@ -1,3 +1,4 @@
+use crate::event::Event;
 use sdl3::Sdl;
 use sdl3::video::Window as Sdl3Window;
 
@@ -14,16 +15,35 @@ impl Default for WindowProps {
             height: 720,
         }
     }
-    pub struct Window {
-        _sdl_context: Sdl,
-        sdl_window: Sdl3Window,
-        pub event_pump: sdl3::EventPump,
-    }
+}
+pub struct Window {
+    _sdl_context: Sdl,
+    sdl_window: Sdl3Window,
+    pub event_pump: sdl3::EventPump,
+}
 
-    impl Window {
-        pub fn new(props: WindowProps) -> Self {
-            let sdl_context = sdl3::init().expect("Failed to initialize SDL3");
+impl Window {
+    pub fn new(props: WindowProps) -> Self {
+        let sdl_context = sdl3::init().expect("Failed to initialize SDL3");
+        let video_subsystem = sdl_context.video().expect("Failed to initialize video");
+        let sdl_window = video_subsystem
+            .window(&props.title, props.width, props.height)
+            .position_centered()
+            .resizable()
+            .build()
+            .expect("Failed to create SDL3 Window");
+
+        let event_pump = sdl_context.event_pump().expect("Failed to get event pump");
+        Self {
+            _sdl_context: sdl_context,
+            sdl_window,
+            event_pump,
         }
     }
-    //TODO: tomorrow
+    pub fn on_update(&mut self) {
+        unimplemented!();
+    }
+    pub fn on_event(&mut self, event: Event) {
+        unimplemented!();
+    }
 }
