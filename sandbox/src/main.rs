ce2cf699d13bb676fbed314618acd5b3e1b3999d
amrxtgh <ghimireamrit619@gmail.com>
2026-08-09 14:09:07 +0545

    clean ups

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,36 +1,4 @@
-use forge::{
-    event::{Event, EventDispatcher, KeyCode, KeyPressedEvent, WindowResizeEvent},
-};
-
-struct Application {
-    running: bool,
-}
-impl Application {
-    pub fn on_event(&mut self, mut event: Event) {
-        println!("Engine Received Event: {:?}", event);
-
-        let mut dispatcher = EventDispatcher::new(&mut event);
-
-        dispatcher.dispatch::<KeyPressedEvent, _>(|e| {
-            println!("Key caught by dispatcher: {:?}", e.key);
-            if e.key == KeyCode::Escape {
-                println!("Escape key hit! Exiting application loop");
-                self.running = false;
-                return true; // Event is marked handled and consumed
-            }
-            false
-        });
-
-        dispatcher.dispatch::<WindowResizeEvent, _>(|e| {
-            println!("Viewport sizing to: {}x{}", e.width, e.height);
-            true
-        });
-    }
-}
-
 fn main() {
-    let sdl_context = sdl3::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("Forge Engine", 1280, 720).position
+    let mut app = forge::Application::new("Forge engine", 1280, 720);
+    app.run();
 }
