36e244efcedf0ff7314a6d2efd025ef2dcfbe03e
amrxtgh <ghimireamrit619@gmail.com>
2026-08-09 14:09:30 +0545

    update: lib.rs

--- forge/src/lib.rs
+++ forge/src/lib.rs
@@ -1,27 +1,40 @@
 pub mod event;
-pub mod window;
-use crate::event::{Event, KeyPressedEvent};
 pub mod logger;
-use logger::Logger;
+pub mod window;
+use sdl3::event::Event as SdlEvent;
+use window::Window;
 
-pub struct Window {
-    pub title: String,
+use crate::window::WindowProps;
+
+pub struct Application {
+    running: bool,
+    window: Window,
 }
-impl Window {
-    pub fn new(title: &str) -> Self {
-        Self {
+impl Application {
+    pub fn new(title: &str, width: u32, height: u32) -> Self {
+        let window = Window::new(WindowProps {
             title: title.to_string(),
+            width,
+            height,
+        });
+        Self {
+            running: true,
+            window,
         }
     }
 
-    // Mocking events
-    pub fn process_events(&mut self) -> Vec<Event> {
-        vec![
-            Event::WindowClose,
-            Event::KeyPressed(KeyPressedEvent {
-                key: event::KeyCode::Escape,
-                repeat_count: 0,
-            }),
-        ]
+    pub fn run(&mut self) {
+        while self.running {
+            let events: Vec<SdlEvent> = self.window.event_pump.poll_iter().collect();
+            for sdl_event in events {
+                self.process_sdl_event(sdl_event);
+            }
+
+            self.window.on_update();
+            std::thread::sleep(std::time::Duration::from_millis(16));
+        }
+    }
+    pub fn process_sdl_event(&mut self, sdl_event: SdlEvent) {
+        unimplemented!();
     }
 }
